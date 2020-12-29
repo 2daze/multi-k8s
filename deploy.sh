f9c71610e79cc11dfb4d74bd21f838038e0a068d
Abner Doubleday <abnerdaviddoubleday@hotmail.com>
2020-12-29 01:04:35 -0800

    finalize travis config.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,18 @@
-docker build -t 2dazed/multi-client -f ./client/Dockerfile ./client
-docker build -t 2dazed/multi-server -f ./server/Dockerfile ./server
-docker build -t 2dazed/multi-worker -f ./worker/Dockerfile ./worker
-docker push 2dazed/multi-client
+docker build -t 2dazed/multi-client:latest -t 2dazed/multi-client:$GIT_SHA  -f ./client/Dockerfile ./client
+docker build -t 2dazed/multi-server:latest -t 2dazed/multi-server:$GIT_SHA -f ./server/Dockerfile ./server
+docker build -t 2dazed/multi-worker:latest -t 2dazed/multi-worker:$GIT_SHA -f ./worker/Dockerfile ./worker
+
+docker push 2dazed/multi-client:latest
+docker push 2dazed/multi-server:latest
+docker push 2dazed/multi-worker:latest
+
+docker push 2dazed/multi-client:$GIT_SHA
+docker push 2dazed/multi-server:$GIT_SHA
+docker push 2dazed/multi-worker:$GIT_SHA
+
 kubectl apply -f k8s
-kubectl set image deployment/server-deployment server=2dazed/multi-server
+
+kubectl set image deployments/client-deployment server=2dazed/multi-client:$GIT_SHA
+kubectl set image deployments/server-deployment server=2dazed/multi-server:$GIT_SHA
+kubectl set image deployments/worker-deployment server=2dazed/multi-worker:$GIT_SHA
 
